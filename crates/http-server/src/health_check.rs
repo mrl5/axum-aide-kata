dd795893516824ae971276701ba86400bc5d6e8c
mrl5 <31549762+mrl5@users.noreply.github.com>
2024-05-24 19:52:18 +0200

    fix: dont cache /health

--- crates/http-server/src/health_check.rs
+++ crates/http-server/src/health_check.rs
@@ -1,6 +1,7 @@
 use aide::{axum::IntoApiResponse, transform::TransformOperation};
 use axum::{response::Json, Extension};
 use common::db::DB;
+use hyper::{header, HeaderMap};
 use schemars::JsonSchema;
 use serde::Serialize;
 
@@ -19,14 +20,20 @@ pub async fn run_healthcheck(Extension(db): Extension<DB>) -> impl IntoApiRespon
     let mut health = Health {
         status: HealthStatus::Healthy,
     };
-
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        header::CACHE_CONTROL,
+        "no-store, no-cache, max-age=0, must-revalidate, proxy-revalidate"
+            .parse()
+            .unwrap(),
+    );
     let db_check = sqlx::query("SELECT 1").fetch_one(&db).await;
 
     if let Err(db_health) = db_check {
         tracing::error!("{:?}", db_health);
         health.status = HealthStatus::Degraded;
     }
-    Json(health)
+    (headers, Json(health))
 }
 
 pub fn run_healthcheck_docs(op: TransformOperation) -> TransformOperation {
